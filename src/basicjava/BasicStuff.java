0b40966437f3efda580d493a5d2b366126a58aa4
bennywags15 <bwagner1@macalester.edu>
2019-09-09 13:48:38 -0500

    ICA 1

---
+++ src/basicjava/BasicStuff.java
@@ -0,0 +1,21 @@
+package basicjava;
+
+public class BasicStuff {
+    public static void main(String[] args){
+        double age0=17;
+        double age1=19;
+        double sumOfAges= age0+age1;
+        System.out.println("We are "+age0+" and "+age1+" years old!");
+        System.out.println("The Sum of our ages is " + (age0+age1) );
+        //System.out.println("The sum of our ages is " + sumOfAges);
+        System.out.println(6/3);
+        System.out.println(6/4);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        int hours= 1600;
+        System.out.println("Weeks:" + hours/168 );
+        System.out.println("Days:" + hours/24);
+        System.out.println("Hours:" + hours);
+
+    }
+}
